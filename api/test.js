a9169334db0edf3fd1ec4678bf5cdea06926310c
Watin <valentde@hotmail.com>
2020-12-23 17:23:11 +0100

    another test

--- api/test.js
+++ api/test.js
@@ -1,5 +1,12 @@
-var con = require('./db_connection.js');
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+    host: "mit.watin.be",
+    user: "valentin",
+    password: "Test123*",
+    database: "mit_api",
+    insecuredAuth : true
+  });
 
 con.connect(function(err) {
     if (err) throw err;
