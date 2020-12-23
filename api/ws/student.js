732ec84eb27c762b7ce08a8fdd27ce99ee8d3ed3
Watin <valentde@hotmail.com>
2020-12-23 14:37:39 +0100

    try

--- api/ws/student.js
+++ api/ws/student.js
@@ -2,7 +2,7 @@ var db		= require('../db.json')
 
 module.exports = {
 	getResults:	function(req, res){
-					},
+                        print("Toto");					},
 	getDetails:	function(req, res){
 						res.json(db);
 					},
