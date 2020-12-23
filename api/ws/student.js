bd62d916b285b0b10acf5af6409c34919ec6fc64
Watin <valentde@hotmail.com>
2020-12-23 16:58:54 +0100

    Test DB

--- api/ws/student.js
+++ api/ws/student.js
@@ -1,8 +1,20 @@
 var db		= require('../db.json')
 
+var con = mysql.createConnection({
+    host: "mit.watin.be",
+    user: "valentin",
+    password: "Test123*",
+    database: "mit_api"
+  });
+
 module.exports = {
 	getResults:	function(req, res){
-                       console.log("Han ouais");
+                        con.connect(function(err) {
+                            if (err) throw err;
+                            con.query("SELECT * FROM customers", function (err, result, fields) {
+                            if (err) throw err;
+                            console.log(result);
+                            });
                         res.send('blabla');
                     },
 	getDetails:	function(req, res){
