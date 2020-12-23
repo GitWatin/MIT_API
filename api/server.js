efc1b4baa563d9531e497a0baf33de21d2a6366e
Watin <valentde@hotmail.com>
2020-12-23 13:04:42 +0100

    Add Server.js

---
+++ api/server.js
@@ -0,0 +1,43 @@
+var express	= require('express');
+var router	= express.Router();
+
+var student	= require('./ws/student')
+
+
+var app = express();
+// GET(récuper)
+// POST(modifier)
+// PUT(creer/envoyer)
+// DELETE(supprimer)
+
+router.get(
+				'/student/:student_id',
+			  student.getDetails
+);
+router.post(
+				'/student/:student_id',
+			  student.setDetails
+);
+router.get(
+				'/student/:student_id/results',
+			  student.getResults
+);
+router.post(
+				'/student/:student_id/results',
+			  student.setResults
+);
+router.put(
+				'/student',
+			  student.create
+);
+
+router.get(
+				'/',
+			  function (req, res){
+				  res.send(" ... list web service")
+			  }
+);
+
+app.use(router);
+
+app.listen(3000);
