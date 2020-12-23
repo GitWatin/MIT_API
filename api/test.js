564a3c160d25a39a596d13ae233979a406d3f6fe
Watin <valentde@hotmail.com>
2020-12-23 17:23:48 +0100

    Change table name

--- api/test.js
+++ api/test.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
+    con.query("SELECT * FROM people", function (err, result, fields) {
       if (err) throw err;
       console.log(result);
     });
