9ecab6d381d7faa1e3ac1b2970ea31b69a9226e5
Watin <valentde@hotmail.com>
2020-12-23 15:08:56 +0100

    t

--- api/ws/student.js
+++ api/ws/student.js
@@ -2,7 +2,9 @@ var db		= require('../db.json')
 
 module.exports = {
 	getResults:	function(req, res){
-                       console.log("Han ouais");					},
+                       console.log("Han ouais");
+                        res.send('blabla');
+                    },
 	getDetails:	function(req, res){
 						res.json(db);
 					},
