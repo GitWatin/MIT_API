7f153d4bf183b470d7b572a8112b1133f4a2829d
Watin <valentde@hotmail.com>
2020-12-23 17:13:43 +0100

    Add test db file

---
+++ api/test.js
@@ -0,0 +1,10 @@
+var db = require('db_connection.js');
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+  });
