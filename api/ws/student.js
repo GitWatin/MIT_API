5e954d0c90a6c0060cb94a18ce95f9a0b4f402f2
Watin <valentde@hotmail.com>
2020-12-26 15:17:51 +0100

    z

--- api/ws/student.js
+++ api/ws/student.js
@@ -5,12 +5,12 @@ var db		= require('../db.json')
 
 module.exports = {
 	getResults:	function(req, res){
-
+						res.send('blabla');
 						con.connect(function(err) {
 							if (err) throw err;
 							con.query("SELECT * FROM people", function (err, result, fields) {
 							if (err) throw err;
-							res.send(result);
+							
 							//console.log(result);
 							});
 						});
