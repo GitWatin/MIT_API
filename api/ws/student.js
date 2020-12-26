8297407184a6f579b02989edf65e081135ff0145
Watin <valentde@hotmail.com>
2020-12-26 15:26:01 +0100

    t

--- api/ws/student.js
+++ api/ws/student.js
@@ -7,7 +7,7 @@ var con = mysql.createConnection(config.databaseConnect);
 
 module.exports = {
 	getResults:	function(req, res){
-						res.send('blabla');
+						//res.send('blabla');
 						con.connect(function(err) {
 							if (err) throw err;
 							con.query("SELECT * FROM people", function (err, result, fields) {
