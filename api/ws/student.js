1dbcf2e7260556dbe1acb45113eadfd3048420e2
Watin <valentde@hotmail.com>
2020-12-27 11:07:59 +0100

    test id

--- api/ws/student.js
+++ api/ws/student.js
@@ -10,7 +10,7 @@ module.exports = {
 						//res.send('blabla');
 						con.connect(function(err) {
 							if (err) throw err;
-							con.query("SELECT * FROM people", function (err, result, fields) {
+							con.query("SELECT * FROM people WHERE id="+req.params.id, function (err, result, fields) {
 							if (err) throw err;
 							res.send(result);
 							console.log(result);
