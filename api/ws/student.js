8e8d27d2dee6c022055350fa7a3c970a28b0014b
Watin <valentde@hotmail.com>
2020-12-27 11:19:15 +0100

    Debug resend

--- api/ws/student.js
+++ api/ws/student.js
@@ -7,7 +7,7 @@ var con = mysql.createConnection(config.databaseConnect);
 
 module.exports = {
 	getResults:	function(req, res){
-						res.send('ID : '+req.params.student_id);
+						//res.send('ID : '+req.params.student_id);
 						con.connect(function(err) {
 							if (err) throw err;
 							con.query("SELECT * FROM people WHERE id="+req.params.student_id, function (err, result, fields) {
