d0a89451e183d18d7fe85619c4a7074ff48df923
root <root@vps-55553594.vps.ovh.net.novalocal>
2020-12-28 20:30:32 +0100

    format files

--- api/test.js
+++ api/test.js
@@ -1,12 +1,11 @@
-var mysql = require('mysql');
-var config = require('./config/db_connection.js');
+var mysql = require("mysql");
+var config = require("./config/db_connection.js");
 var con = mysql.createConnection(config.databaseConnect);
 
-
-con.connect(function(err) {
+con.connect(function (err) {
+  if (err) throw err;
+  con.query("SELECT * FROM people", function (err, result, fields) {
     if (err) throw err;
-    con.query("SELECT * FROM people", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
+    console.log(result);
   });
+});
