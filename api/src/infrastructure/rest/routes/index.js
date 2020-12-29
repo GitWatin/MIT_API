d4b465c27f164a5d96689fce6d533805139d1b16
GitWatin <valentde@hotmail.com>
2020-12-29 17:07:41 +0100

    add gcc router to api router

--- api/src/infrastructure/rest/routes/index.js
+++ api/src/infrastructure/rest/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const studentRouter = require("./student");
 const teacherRouter = require("./teacher");
+const gccRouter = require("./gcc");
 
 router.use("/student", studentRouter);
 router.use("/teacher", teacherRouter);
+router.use("/gcc", gccRouter);
 
 module.exports = router;
