689409003c5294c490073bceced4e7cdf7caee1c
GitWatin <valentde@hotmail.com>
2020-12-29 22:19:28 +0100

    remove multer + add verification

--- api/src/infrastructure/rest/routes/gcc.js
+++ api/src/infrastructure/rest/routes/gcc.js
@@ -1,6 +1,5 @@
 const os = require("os");
 const gccRouter = require("express").Router();
-const multer = require("multer");
 const { compile, compileStatus } = require("../../gcc/gcc");
 const fs = require("fs");
 const unixTimestamp = require("../../../util/timestamp");
@@ -9,105 +8,107 @@ const { extension } = require("../../../util/file");
 // Liste des extensions authorisées.
 const ASSET_TYPES = { c: true, cpp: true, asm: true };
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, os.tmpdir());
-  },
-  filename: function (req, file, cb) {
-    const user = req.locals.user;
-    const ext = extension(file.originalname);
-    const fileName = `${unixTimestamp()}_${user.email}.${ext}`;
-    cb(null, fileName);
-  },
-});
-const fileFilter = (req, file, cb) => {
-  const ext = extension(file.originalname);
-  if (!ASSET_TYPES[ext.toLowerCase()]) {
-    // Je rejette le fichier
-    // https://github.com/expressjs/multer#filefilter
-    cb(null, false);
-    // Je déclenche une erreur
-    // https://github.com/expressjs/multer#error-handling
-    cb(new Error("File Type unsupported."));
-  } else {
-    // J'accepte le fichier
-    cb(null, true);
-  }
-};
-const upload = multer({ storage, fileFilter });
-
 // GET(récuper)
 // POST(modifier)
 // PUT(creer/mettre à jour totalement)
 // DELETE(supprimer)
 
+const extractFileName = (req, res, next) => {
+  const { name } = req.params;
+
+  if (!name) {
+    return res.status(400).send({ msg: "You have to enter a name." });
+  }
+
+  return next();
+};
+
+const extractExtension = (req, res, next) => {
+  const { name } = req.params;
+
+  const ext = extension(name);
+
+  if (!ext) {
+    return res.status(400).send({
+      msg: "The name of the file doesn't contains any extension.",
+    });
+  }
+
+  const file = req.locals.file || {};
+  req.locals = {
+    ...req.locals,
+    file: { ...file, extension: ext },
+  };
+
+  return next();
+};
+
+const isExtensionSupported = (req, res, next) => {
+  const { extension: ext } = req.locals.file;
+
+  if (!ASSET_TYPES[ext.toLowerCase()]) {
+    return res
+      .status(400)
+      .send({ msg: "The file extension is not supported." });
+  }
+
+  return next();
+};
+
 gccRouter.post(
-  "/",
-  (req, res, next) => {
+  "/:name",
+  extractFileName,
+  extractExtension,
+  isExtensionSupported,
+  (_req, res, next) => {
     if (compileStatus.isCompiling()) {
-      return res.status(423).send("Please wait a little wait bit. Take a ☕️");
+      return res
+        .status(423)
+        .send({ msg: "Please wait a little wait bit. Take a ☕️" });
     }
+
     return next();
   },
   (req, res, next) => {
     // Une méthode async permet d'utiliser le await
     // qui bloque le code pour attendre le résultat d'une
     // promesse.
-    upload.single("file")(req, res, (err) => {
-      if (err) {
-        return res.status(400).send({ msg: err.message });
-      }
-      return next();
-    });
+    const {
+      user,
+      file: { extension: ext },
+    } = req.locals;
+
+    const content = req.body;
+    const tmp = os.tmpdir;
+    const fileName = `${unixTimestamp()}_${user.email}`;
+    const path = `${tmp}/${fileName}.${ext}`;
+    const file = req.locals.file;
+    try {
+      fs.writeFileSync(path, content);
+      req.locals = {
+        ...req.locals,
+        file: { ...file, path },
+      };
+    } catch (err) {
+      return res
+        .status(400)
+        .send({ msg: `Unable to write the file because of ${err.message}` });
+    }
+    return next();
   },
   async (req, res) => {
-    const { path } = req.file;
+    const {
+      file: { path },
+    } = req.locals;
 
     try {
       const result = await compile(path);
       return res.status(200).send(result);
     } catch (err) {
       compileStatus.stop();
-      return res.status(500).send({ msg: "Unable to run your file." });
-    }
-  }
-);
-
-gccRouter.post(
-  "/toto/:name",
-  (req, res, next) => {
-    if (compileStatus.isCompiling()) {
-      return res.status(423).send("Please wait a little wait bit. Take a ☕️");
-    }
-    return next();
-  },
-  (req, res, next) => {
-    // Une méthode async permet d'utiliser le await
-    // qui bloque le code pour attendre le résultat d'une
-    // promesse.
-    const { name } = req.params;
-    const ext = extension(name);
-    const content = req.body;
-    const tmp = os.tmpdir;
-    const fileName = Math.random().toString(36).substring(7);
-    const path = `${tmp}/${fileName}.${ext}`;
-    try {
-      fs.writeFileSync(path, content);
-      req.locals = { ...req.locals, file: path };
-    } catch (err) {
-      return res.status(400).send({ msg: "Try again" });
-    }
-    return next();
-  },
-  async (req, res) => {
-    const { file } = req.locals;
-
-    try {
-      const result = await compile(file);
-      return res.status(200).send(result);
-    } catch (err) {
-      compileStatus.stop();
-      return res.status(500).send({ msg: "Unable to run your file." });
+      return res
+        .status(500)
+        .send({ msg: `Unable to run your file because of: ${err.message}` });
     }
   }
 );
